a30a74c09f75a60244ee368c67c62f8b839b2a6e
Takafumi Moriyama <taka.youthful.426@gmail.com>
2023-01-12 09:34:24 +0900

    dev_err処理追加。titleのみ取得する処理実装

--- app/curlUrl.go
+++ app/curlUrl.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	res, _ := http.Get("https://qiita.com")
+	res, err := http.Get("https://qiita.com/sayama0402/items/e32814e38375fafa919a")
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer res.Body.Close()
-	byteArray, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(byteArray))
+
+	byteArray, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	siteTitle := ""
+	if strings.Contains(string(byteArray), "<title>") {
+		splitArray := strings.Split(string(byteArray), "<title>")
+		splitArray = strings.Split(splitArray[1], "</title>")
+		siteTitle = splitArray[0]
+	}
+	fmt.Println(siteTitle)
 }
