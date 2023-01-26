933f10e086a1c6829cdae58181fbfb1d80eea81c
Takafumi Moriyama <taka.youthful.426@gmail.com>
2023-01-26 21:02:11 +0900

    dev_request用のjsonファイルを読み取り、notionAPIを実行する処理実装

---
+++ app/postNotionApiStockArticle.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type PostNotionApiStockArticleRequestData struct {
+	Parent struct {
+		DatabaseID string
+	} `json:"parent"`
+	Properties struct {
+		Title struct {
+			Title []struct {
+				Text struct {
+					Content string `json:"content"`
+				} `json:"text"`
+			} `json:"title"`
+		} `json:"title"`
+		Status struct {
+			Select struct {
+				Name string `json:"name"`
+			} `json:"select"`
+		} `json:"status"`
+		URL struct {
+			Url string `json:"URL"`
+		} `json:"URL"`
+	} `json:"properties"`
+}
+
+func main() {
+	// TODO: structを用いたbody
+	// data := PostNotionApiStockArticleRequestData{
+	// 	Parent: struct {
+	// 		DatabaseID string
+	// 	}{
+	// 		DatabaseID: "NOTION_DATABASE_ID",
+	// 	},
+	// 	Properties: struct {
+	// 		Title struct {
+	// 			Title []struct {
+	// 				Text struct {
+	// 					Content string `json:"content"`
+	// 				} `json:"text"`
+	// 			} `json:"title"`
+	// 		} `json:"title"`
+	// 		Status struct {
+	// 			Select struct {
+	// 				Name string `json:"name"`
+	// 			} `json:"select"`
+	// 		} `json:"status"`
+	// 		URL struct {
+	// 			Url string `json:"URL"`
+	// 		} `json:"URL"`
+	// 	}{
+	// 		Title: struct {
+	// 			Title []struct {
+	// 				Text struct {
+	// 					Content string `json:"content"`
+	// 				} `json:"text"`
+	// 			} `json:"title"`
+	// 		}{
+	// 			Title: []struct {
+	// 				Text struct {
+	// 					Content string `json:"content"`
+	// 				} `json:"text"`
+	// 			}{
+	// 				{
+	// 					Text: struct {
+	// 						Content string `json:"content"`
+	// 					}{
+	// 						Content: "content",
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 		Status: struct {
+	// 			Select struct {
+	// 				Name string `json:"name"`
+	// 			} `json:"select"`
+	// 		}{
+	// 			Select: struct {
+	// 				Name string `json:"name"`
+	// 			}{
+	// 				Name: "name",
+	// 			},
+	// 		},
+	// 		URL: struct {
+	// 			Url string `json:"URL"`
+	// 		}{
+	// 			Url: "url",
+	// 		},
+	// 	},
+	// }
+	jsonBody, err := os.ReadFile("postNotionApiStockArticleRequest.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	relacedJsonBody := replaceParameter(string(jsonBody), "置換後content", "置換後url")
+
+	body := strings.NewReader(string(relacedJsonBody))
+	req, err := http.NewRequest("POST", "https://api.notion.com/v1/pages", body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("NOTION_KEY"))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Notion-Version", "2022-06-28")
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+}
+
+func replaceParameter(jsonBody, content, url string) string {
+	replacement := map[string]string{
+		"%NOTION_DATABASE_ID%": os.Getenv("NOTION_DATABASE_ID"),
+		"%CONTENT%":            content,
+		"%SITEURL%":            url,
+	}
+	for key, value := range replacement {
+		jsonBody = strings.Replace(jsonBody, key, value, -1)
+	}
+	return jsonBody
+}
